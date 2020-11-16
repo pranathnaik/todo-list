02203722e69aac33f63427ec7c34bb2f5268f8d2
pranathnaik <pranathnaik12@gmail.com>
2020-11-16 20:13:31 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,65 +1,72 @@
-import React,{useState} from "react"
-import ToDoList from "./ToDoList"
-import "./style.css"
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import IconButton from '@material-ui/core/IconButton';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+import "./style.css";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
 
-const App =() =>{
-
+const App = () => {
   const [inputList, setInputList] = useState("");
-  const [Items,setItems] = useState([]);
+  const [Items, setItems] = useState([]);
 
-  const itemEvent =(event)=> {
+  const itemEvent = (event) => {
     setInputList(event.target.value);
   };
 
-  const listOfItems =()=>{
-    setItems((oldItems)=>{
-      return [...oldItems,inputList];
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
     });
 
-    setInputList('')
-  }
+    setInputList("");
+  };
 
-  const deleteItems =(id)=>{
-
-    setItems((oldItems)=>{
-      return oldItems.filter((arrElem,index)=>{
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
         return index !== id;
-      })
-    })
-}
+      });
+    });
+  };
 
-  return (<>
-    <div className='main_div'>
-      <div className='center_div'>
-      
-        <br/>
-        <div className="items">
-        <h1> Todo List</h1>
-        <br/>
-          <div className="input">
-            <TextField value={inputList}  onChange={itemEvent} color="secondary" label="Add Todos" variant="outlined" />
-            <IconButton onClick={listOfItems} >
-            <AddCircleIcon fontSize="large" />
-            </IconButton>
-          </div>
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <div className="items">
+            <h1> Todo List</h1>
+            <br />
+            <div className="input">
+              <TextField
+                value={inputList}
+                onChange={itemEvent}
+                color="secondary"
+                label="Add Todos"
+                variant="outlined"
+              />
+              <IconButton onClick={listOfItems}>
+                <AddCircleIcon fontSize="large" />
+              </IconButton>
+            </div>
 
-        <ol>
-          {Items.map((itemval,index)=>{
-          return   <ToDoList 
-                      key={index}
-                      id={index}
-                      text ={itemval}
-                      onSelect ={deleteItems}
+            <ol>
+              {Items.map((itemval, index) => {
+                return (
+                  <ToDoList
+                    key={index}
+                    id={index}
+                    text={itemval}
+                    onSelect={deleteItems}
                   />
-          })}
-        </ol>
+                );
+              })}
+            </ol>
+          </div>
         </div>
-        </div>
-    </div>
-  </>)
-}
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,29 +1,30 @@
-import React from "react"
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
 
-
-
-const ToDoList =(props)=>{
-
-    return (
-        <>
-        <div className="list">
-            <ListItem>
-                  <ListItemText
-                    primary={props.text}   
-                  />
-                  <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="delete" onClick={()=>{ props.onSelect(props.id)}} >
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-            </ListItem>         
-        </div>
-        </>
-    )
-}
-export default ToDoList
+const ToDoList = (props) => {
+  return (
+    <>
+      <div className="list">
+        <ListItem>
+          <ListItemText primary={props.text} />
+          <ListItemSecondaryAction>
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={() => {
+                props.onSelect(props.id);
+              }}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      </div>
+    </>
+  );
+};
+export default ToDoList;
